16b3a7e7d968d0fcecea30b38400fcde17c7f80d
Don Honerbrink <dhonerbrink@gmail.com>
2015-01-30 16:38:04 -0600

    Updating all files with licensing information.

--- RetroFE/Source/Graphics/Animate/Tween.cpp
+++ RetroFE/Source/Graphics/Animate/Tween.cpp
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with RetroFE.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "Tween.h"
 #include <algorithm>
 #define _USE_MATH_DEFINES
