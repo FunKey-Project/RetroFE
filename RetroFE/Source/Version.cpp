63b80e84200131fb71f44c3501dcabd87c2a52fb
Pieter Hulshoff <pieter@towel42.nl>
2018-05-29 22:19:05 +0200

    Updated version to 0.8.16.

--- RetroFE/Source/Version.cpp
+++ RetroFE/Source/Version.cpp
@@ -21,7 +21,7 @@
 
 std::string retrofe_version_major = "0";
 std::string retrofe_version_minor = "8";
-std::string retrofe_version_build = "15b10";
+std::string retrofe_version_build = "16";
 
 
 std::string Version::getString( )
