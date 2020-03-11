834038bf51b2d9af84a5c03a3125a41c478ad426
Pieter Hulshoff <pieter@towel42.nl>
2020-03-11 15:43:59 +0100

    Fixed sorting for .sub files based on code by procyon.

--- RetroFE/Source/RetroFE.cpp
+++ RetroFE/Source/RetroFE.cpp
@@ -1609,10 +1609,11 @@ CollectionInfo *RetroFE::getCollection(std::string collectionName)
     }
     closedir( dp );
 
-    collection->sortItems( );
-
     bool menuSort = true;
     config_.getProperty( "collections." + collectionName + ".list.menuSort", menuSort );
+
+    if (menuSort)
+        collection->sortItems( );
 
     MenuParser mp;
     mp.buildMenuItems( collection, menuSort);
