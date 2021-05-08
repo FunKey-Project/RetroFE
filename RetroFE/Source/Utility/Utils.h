7fc674e466763ec8dfd1e916c5b0959ffc001566
Vincent-FK <vincent.buso@funkey-project.com>
2021-05-08 23:04:15 +0200

    rename start_audio_amp to audio_amp
    Signed-off-by: Vincent-FK <vincent.buso@funkey-project.com>

--- RetroFE/Source/Utility/Utils.h
+++ RetroFE/Source/Utility/Utils.h
@@ -23,8 +23,8 @@
 #define SHELL_CMD_ROOTFS_RW             "rw"
 #define SHELL_CMD_ROOTFS_RO             "ro"
 #define SHELL_CMD_RECORD_PID            "record_pid"
-#define SHELL_CMD_TURN_AMPLI_ON         "start_audio_amp 1"
-#define SHELL_CMD_TURN_AMPLI_OFF        "start_audio_amp 0"
+#define SHELL_CMD_TURN_AMPLI_ON         "audio_amp on"
+#define SHELL_CMD_TURN_AMPLI_OFF        "audio_amp off"
 #define SHELL_CMD_MAPPING_ROM           "keymap rom"
 #define SHELL_CMD_MAPPING_DEFAULT       "keymap default"
 
